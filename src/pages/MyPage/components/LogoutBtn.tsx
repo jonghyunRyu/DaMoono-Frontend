eb7ec310e1360d81ce04b557e453a3d0adb64471
JongHyun Ryu <willowise@naver.com>
2026-01-23 11:23:36 +0900

    chore: 필요없는 코드 삭제

--- src/pages/MyPage/components/LogoutBtn.tsx
+++ src/pages/MyPage/components/LogoutBtn.tsx
@@ -1,9 +1,5 @@
 import * as css from '../styles/MyPage.css';
 
-interface LogoutButtonProps {
-  onLogout: () => void;
-}
-
 export function LogoutBtn() {
   return (
     <button type="button" className={css.logout}>
